d814ece1a74c762b2e4e1045508af7749893ea38
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-07-23 14:01:48 -0300

    Ajustes

--- server/src/config/multer.js
+++ server/src/config/multer.js
@@ -1,9 +1,11 @@
 const multer = require('multer');
 const path = require('path');
 
+require('')
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../assets/');
+    cb(null, path.join(__dirname, '../assets/'));
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}_${file.originalname}`);

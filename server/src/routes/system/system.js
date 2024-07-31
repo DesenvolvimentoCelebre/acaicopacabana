fa53eacfa475400a74c3ed623d6a7e6789fe54a7
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-07-31 09:47:42 -0300

    Adicao de rota para saldo

--- server/src/routes/system/system.js
+++ server/src/routes/system/system.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { getcaixa } = require('../../service/system');
+const { getcaixa, saldo } = require('../../service/system');
 const { errorMiddleware } = require('../../utils/intTelegram');
 
 const system = express.Router();
@@ -8,6 +8,20 @@ const system = express.Router();
 system.get("/gcx", async (req, res) => {
     try {
       const result = await getcaixa();
+      
+      if (result.success) {
+        res.status(200).json(result);
+      } else {
+        res.status(500).json(result);
+      }
+    } catch (error) {
+      res.status(500).json({ success: false, error: ["Erro interno do servidor", error]});
+    }
+});
+
+system.get("/sd", async (req, res) => {
+    try {
+      const result = await saldo();
       
       if (result.success) {
         res.status(200).json(result);
--- server/src/service/system.js
+++ server/src/service/system.js
@@ -16,6 +16,33 @@ async function getcaixa() {
     }
   }
 
+async function saldo() {
+  try {
+    const query = `
+SELECT
+	sum(pedno.valor_unit) as saldo
+FROM 
+	pedno
+INNER JOIN
+	pay
+ON
+	pedno.pedido = pay.pedido
+WHERE
+	pedno.bit2 = 1 AND pay.tipo = 0`;
+
+  const [results] = await pool.query(query);
+  
+      if (results.length === 0) {
+        return { success: true, message: "Não foi encontrado nenhum produto com esse nome" };
+      } else {
+        return { success: true, message: results };
+      }
+    } catch (error) {
+      return { success: false, error: "Erro no servidor, por favor contatar o administrador", details: error };
+    }
+  }
+
 module.exports = {
-    getcaixa
+    getcaixa,
+    saldo
 }

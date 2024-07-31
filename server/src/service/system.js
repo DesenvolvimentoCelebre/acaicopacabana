273ed79c40e4560354bf50ecb3aa61b587013e73
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-07-31 08:35:28 -0300

    Adicao de rota para verificar se o caixa está aberto

--- server/src/service/system.js
+++ server/src/service/system.js
@@ -1,10 +1,10 @@
 const pool  = require('../database/connection');
 
-async function getcaixa({s0, sd}) {
+async function getcaixa() {
     try {
       const query = `SELECT s0 FROM cxlog WHERE date =  CURDATE()`;
 
-      const [results] = await pool.query(query, values);
+      const [results] = await pool.query(query);
   
       if (results.length === 0) {
         return { success: true, message: "Não foi encontrado nenhum produto com esse nome" };

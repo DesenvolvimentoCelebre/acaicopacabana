1e4fc60321f64f565a30352440508d4a85d5a1b9
DesenvolvimentoCelebre <161392044+DesenvolvimentoCelebre@users.noreply.github.com>
2024-08-05 15:52:06 -0300

    Update system.js

--- server/src/routes/system/system.js
+++ server/src/routes/system/system.js
@@ -1,52 +1,112 @@
 const express = require("express");
 
-const { getcaixa, saldo, abrirCaixa } = require('../../service/system');
+const { getcaixa, saldo, abrirCaixa, fechamento, relDiario } = require('../../service/system');
 const { errorMiddleware } = require('../../utils/intTelegram');
 
 const system = express.Router();
 
 system.get("/gcx", async (req, res) => {
     try {
-      const result = await getcaixa();
-      
-      if (result.success) {
-        res.status(200).json(result);
-      } else {
-        res.status(500).json(result);
-      }
+        const { userno } = req.query
+        const result = await getcaixa(userno);
+
+        if (result.success) {
+            res.status(200).json(result);
+        } else {
+            res.status(500).json(result);
+        }
     } catch (error) {
-      res.status(500).json({ success: false, error: ["Erro interno do servidor", error]});
+        res.status(500).json({ success: false, error: ["Erro interno do servidor", error] });
     }
 });
 
 system.get("/sd", async (req, res) => {
     try {
-      const result = await saldo();
-      
-      if (result.success) {
-        res.status(200).json(result);
-      } else {
-        res.status(500).json(result);
-      }
+        const { userno } = req.query;
+        const result = await saldo(userno);
+
+        if (result.success) {
+            res.status(200).json(result);
+        } else {
+            res.status(500).json(result);
+        }
     } catch (error) {
-      res.status(500).json({ success: false, error: ["Erro interno do servidor", error]});
+        res.status(500).json({ success: false, error: ["Erro interno do servidor", error] });
     }
 });
 
-system.get("/opc", async (req, res) => {
+system.post("/opc", async (req, res) => {
     try {
-      const result = await abrirCaixa();
+      const {s0, sd, userno} = req.body
+      const result = await abrirCaixa(s0, sd, userno);
       
       if (result.success) {
         res.status(200).json(result);
       } else {
         res.status(500).json(result);
       }
+
     } catch (error) {
-      res.status(500).json({ success: false, error: ["Erro interno do servidor", error]});
+        res.status(500).json({ success: false, error: ["Erro interno do servidor", error] });
     }
 });
-  
-  system.use(errorMiddleware)
 
-  module.exports = system;
+system.post("/fechamento", async (req, res) => {
+  try {
+      const { userno } = req.body;
+      
+      if (!userno) {
+          return res.status(400).json({ success: false, error: ["userno é necessário"] });
+      }
+      
+      const result = await fechamento(userno);
+      
+      if (result.success) {
+          res.status(200).json(result);
+      } else {
+          res.status(500).json(result);
+      }
+  } catch (error) {
+      res.status(500).json({ success: false, error: ["Erro interno do servidor", error.message] });
+  }
+});
+
+system.get("/rdiario", async (req, res) => {
+    try {
+
+      const { userno } = req.query
+      const result = await relDiario(userno);
+      
+      if (result.success) {
+        res.status(200).json(result);
+      } else {
+        res.status(500).json(result);
+      }
+
+    
+
+    } catch (error) {
+        res.status(500).json({ success: false, error: ["Erro interno do servidor", error] });
+    }
+});
+
+system.get("/rdiario", async (req, res) => {
+    try {
+
+        const { userno } = req.query;
+        const result = await relDiario(userno);
+
+        if (result.success) {
+            res.status(200).json(result);
+        } else {
+            res.status(500).json(result);
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ success: false, error: ["Erro interno do servidor", error] });
+    }
+});
+
+system.use(errorMiddleware)
+
+module.exports = system;

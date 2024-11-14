039341ca4599b77ae6d4a7d790e549cd59472d59
CAIQUE SILVA DOS SANTOS CARVALHO <caique.carvalho@baianao.corp>
2024-11-14 07:54:10 -0300

    Adição rota para add saldo

--- server/src/service/system.js
+++ server/src/service/system.js
@@ -291,7 +291,7 @@ WHERE NOT EXISTS (
     const sangria = resultSangria[0].sangria;    
     
     // novo saldo inicial 
-    const sdinicial = "SELECT sd_old as sdinicial FROM s_log WHERE user_cx = ? AND date = current_date" | "0";
+    const sdinicial = "SELECT sd_old as sdinicial FROM s_log WHERE user_cx = ? AND date = current_date";
     const [SdinicialTrue] = await pool.query(sdinicial, [userno]);
     const sdinicialFalse = "SELECT sd as sdinicial FROM cxlog WHERE date = current_date AND s0 = 1 AND userno = ?"
     const [SdinicialFalse] = await pool.query(sdinicialFalse, [userno]);
@@ -299,9 +299,9 @@ WHERE NOT EXISTS (
     let rdiario_saldoinicial
     
     if (SdinicialTrue.length > 0) {
-      rdiario_saldoinicial = SdinicialTrue[0].sdinicial
+      rdiario_saldoinicial = SdinicialTrue[0].sdinicial ?? 0
     } else {
-      rdiario_saldoinicial = SdinicialFalse[0].sdinicial
+      rdiario_saldoinicial = SdinicialFalse[0].sdinicial ?? 0
     }    
     
      const caixaDoDia = {
